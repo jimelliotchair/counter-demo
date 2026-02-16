7c8c4c374e4a649d5c1fa85c75b4592bf7b0c8ae
JimElliotchair <jimelliot.c@gmail.com>
2026-02-16 10:10:28 +0530

    Initial Flutter web deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "762376723" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "962042431" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
